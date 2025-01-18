64b8c5c6f867307edb187ca68285ea42a2aa3888
bencohendev <ben@bencohen.dev>
2025-01-18 15:58:28 -0800

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,13 @@
 </svelte:head>
 
 <div class=" flex h-full flex-col justify-between">
-	<section class="text-home-cream flex flex-col items-center justify-center py-8">
+	<section class="flex flex-col items-center justify-center py-8 text-home-cream">
 		<h1 class="mb-8 w-full text-center text-6xl">Ben Cohen-Chen</h1>
 		<p class="mb-4 text-xl font-normal">Typescript | React | Svelte</p>
-		<p class="text-4xl text-white">Let's talk: ben@bencohen.dev</p>
+		<p class="text-white text-4xl">Let's talk: ben@bencohen.dev</p>
 	</section>
 	<section class="flex flex-col items-center justify-center pb-8">
-		<div class="text-home-orange active:text-home-blue text-lg">
+		<div class="text-lg text-home-orange active:text-home-blue">
 			<a class="underline" href="https://theme.bencohen.dev">theme generator</a>
 			<span>|</span>
 			<a class="underline" href="https://pitch.bencohen.dev">pitch generator</a>
