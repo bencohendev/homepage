b7ef49e614dc09bc388bcb1e004785e2a3517cb3
bencohendev <ben@bencohen.dev>
2025-01-18 16:09:03 -0800

    tailwind extend colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,15 +4,17 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: { fontFamily: { grotesk: ['Space Grotesk'] } },
-		colors: {
-			home: {
-				purple: '#372554',
-				cream: '#FEFFEC',
-				orange: '#ef8354',
-				yellow: 'f1d302',
-				blue: '508991'
-			}
+		extend: {
+			colors: {
+				home: {
+					purple: '#372554',
+					cream: '#FEFFEC',
+					orange: '#ef8354',
+					yellow: '#f1d302',
+					blue: '#508991'
+				}
+			},
+			fontFamily: { grotesk: ['Space Grotesk'] }
 		}
 	},
 
