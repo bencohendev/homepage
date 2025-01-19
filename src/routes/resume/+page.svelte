8bd19f820a3c3623c4906a9661af96cbc01e41c1
bencohendev <ben@bencohen.dev>
2025-01-19 10:57:01 -0800

    basic job content

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,6 +1,18 @@
+<script>
+	import Job from './components/job/Job.svelte';
+
+	let { data } = $props();
+	const { jobs } = data;
+</script>
+
 <div class="flex flex-col items-center justify-center text-home-cream">
 	<section>
 		<h2>Experience</h2>
+		<div>
+			{#each jobs as job}
+				<Job {job} />
+			{/each}
+		</div>
 	</section>
 	<section>
 		<h2>Skills</h2>
---
+++ src/routes/resume/components/job/Job.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { Job } from '../../proxy+page';
+
+	export let job: Job;
+	const { company, title, link, date, subhead, bullets } = job;
+</script>
+
+<section class="text-home-cream">
+	<div class="flex flex-col">
+		<h3>
+			<a href={link}>
+				{company}
+			</a>
+		</h3>
+		<div class="flex justify-between">
+			<div>
+				{title}
+			</div>
+			<div>
+				{date}
+			</div>
+		</div>
+		<div>
+			{subhead}
+		</div>
+		<ul class="flex flex-col">
+			{#each bullets as bullet}
+				<li class="ml-2">
+					{bullet.text}
+				</li>
+				{#if bullet?.subPoints?.length}
+					{#each bullet.subPoints as subPoint}
+						<li class="ml-4">
+							{subPoint.text}
+						</li>
+					{/each}
+				{/if}
+			{/each}
+		</ul>
+	</div>
+</section>
