18b664fafee13faf35e34718f52be42c537f88b2
bencohendev <ben@bencohen.dev>
2025-01-19 08:41:59 -0800

    bar components

---
+++ src/lib/components/ThickBar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+
+	export let variant: 'primary' | 'secondary' = 'primary';
+	export let flyDirection: 'left' | 'right' = 'left';
+
+	const color = variant === 'primary' ? 'bg-home-purple' : 'bg-home-orange';
+	const flyX = flyDirection === 'left' ? -1000 : 1000;
+</script>
+
+<div in:fly={{ x: flyX }} class={`${color} h-4 w-full`}></div>
---
+++ src/lib/components/ThinBar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let variant: 'primary' | 'secondary' = 'primary';
+
+	const color = variant === 'primary' ? 'bg-home-purple' : 'bg-home-orange';
+</script>
+
+<hr class={`${color}h-1`} />
