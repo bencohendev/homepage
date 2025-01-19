e430542f0b576835a74a34afe5225b45476284fb
bencohendev <ben@bencohen.dev>
2025-01-19 09:33:31 -0800

    resume scaffolding

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,1 +1,8 @@
-<section>resume</section>
+<div class="flex flex-col items-center justify-center text-home-cream">
+	<section>
+		<h2>Experience</h2>
+	</section>
+	<section>
+		<h2>Skills</h2>
+	</section>
+</div>
