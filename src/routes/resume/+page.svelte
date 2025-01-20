9258b1341f079a9ee169a84b1cb4473289916cc7
bencohendev <ben@bencohen.dev>
2025-01-19 16:45:07 -0800

    downloadable version of resume

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { Download } from 'lucide-svelte';
 	import ThinBar from '$lib/components/ThinBar.svelte';
 	import Job from './components/Job.svelte';
 	import Skills from './components/Skills.svelte';
@@ -7,6 +8,11 @@
 	const { jobs, skills } = data;
 </script>
 
+<a
+	class="mb-4 flex px-12 text-home-cream"
+	href="/Resume_Ben_Cohen-Chen.pdf"
+	download="Resume_Ben_Cohen-Chen.pdf">Download as PDF <Download color={'white'} /></a
+>
 <div class="flex justify-between gap-12 px-12 text-home-cream">
 	<section class="">
 		<div class="mb-4">
