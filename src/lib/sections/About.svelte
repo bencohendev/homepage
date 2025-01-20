d80445ec4f06d71cd5af3ad9eadfeacfbf9f50f4
bencohendev <ben@bencohen.dev>
2025-01-19 16:52:32 -0800

    basic about section

--- src/lib/sections/About.svelte
+++ src/lib/sections/About.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
-	import Card from '$lib/components/Card.svelte';
+	import ThinBar from '$lib/components/ThinBar.svelte';
 </script>
 
-<Card>
-	<div class="p-8 text-home-cream">This is the about section</div>
-</Card>
+<section class="flex flex-col items-center gap-12 text-home-cream">
+	<h2 class="w-fit text-4xl font-bold">About <ThinBar /></h2>
+	<p class="w-1/2 text-home-cream">
+		Coding started as a means to an end, but has become my passion Shortly after college I moved to
+		NYC to pursue my dreams of becoming a jazz saxophonist. After a long stint of restaurant work to
+		make ends meet, I started learning web development as a way to support myself between gigs. Then
+		came the pandemic and there were no more gigs. I decided to get more serious about code At this
+		time, I was ready to move on from the platform e-commerce work I'd been doing with Codacity and
+		work on something more custom. Antidote Games provided me with a great opportunity to focus on
+		sharpening my JS chops and start building and deploying more sophisticated custom websites.
+		After a year with Antidote, I was ready to take the next big step Since then, I've built a
+		variety of sophisticated, cutting edge products that solve real world problems for users. I've
+		also found I have a deep passion for writing good clean code and for collaborating with teams of
+		highly skilled engineers, designers, and product engineers, all with the end user firmly in
+		mind. Using my firm foundations in Typescript, Javascript, React, Svelte, Angular, Git, Node,
+		AWS and a variety of other tools and platforms, I love working in Scrum and Agile environments.
+		I'm always looking for ways to increase efficiencies through time tested processes to speed up
+		delivery of excellent features. And I have a proven track record of building successful products
+		that have led to massive increases in user bases and revenue for my clients. So let's get in
+		touch and see what we can build together!
+	</p>
+</section>
