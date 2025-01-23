631b6651d4a7c1e7718482c53b8ddb797c7b7264
bencohendev <ben@bencohen.dev>
2025-01-22 17:04:31 -0800

    color changes

--- src/lib/sections/Intro.svelte
+++ src/lib/sections/Intro.svelte
@@ -1,10 +1,10 @@
 <section class="flex flex-col items-center gap-6 p-8">
 	<p class="w-full text-center text-lg sm:w-1/2">
 		I believe collaboration is the key to building excellent software. Nothing makes me happier than
-		<span class="font-bold text-home-blue">learning</span> something,
-		<span class="font-bold text-home-blue">teaching</span>
-		something, <span class="font-bold text-home-blue">fixing</span> something,
-		<span class="font-bold text-home-blue">building</span> something
+		<span class="font-bold text-home-yellow">learning</span> something,
+		<span class="font-bold text-home-yellow">teaching</span>
+		something, <span class="font-bold text-home-yellow">fixing</span> something,
+		<span class="font-bold text-home-yellow">building</span> something
 	</p>
 	<div class="flex w-full justify-center">
 		<img
@@ -13,7 +13,7 @@
 			alt="A stylized cartoon of Ben sitting in front of a field of tulips"
 		/>
 	</div>
-	<p class="text-white mb-12 text-2xl">
+	<p class="mb-12 text-2xl text-home-orange hover:scale-110">
 		Let's talk: <a href="mailto:ben@bencohen.dev">ben@bencohen.dev</a>
 	</p>
 </section>
